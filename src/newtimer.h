bb7e13b98883c86a5c0dd8538f1d15778a41022d
Shaharyar Khalid <shaharyar@midway2-login1.rcc.local>
2021-08-19 22:29:07 -0500

    changed

---
+++ src/newtimer.h
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <sys/time.h>
+
+void StartTimer();
+double GetTimer();
+
+
+
+struct timeval timerStart;
+
+void StartTimer(){
+  gettimeofday(&timerStart, NULL);
+}
+
+double GetTimer(){
+  struct timeval timerStop, timerElapsed;
+  gettimeofday(&timerStop, NULL);
+  timersub(&timerStop, &timerStart, &timerElapsed);
+
+  return timerElapsed.tv_sec*1000.0+timerElapsed.tv_usec/1000.0;
+    
+}
