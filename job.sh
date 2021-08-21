0e9e9bee9db1285bba8c9963fac6574b25969970
Shaharyar Khalid <shaharyar@midway2-login1.rcc.local>
2021-08-21 13:27:25 -0500

    modified

--- job.sh
+++ job.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 module load cuda
+module load papi
 #sinteractive --account=mpcs52018 --exclusive --partition=gpu2 --nodes=1 --time=00:10:00
 sinteractive --exclusive --partition=gpu2 --nodes=1 --gres=gpu:1 --account=mpcs52018
